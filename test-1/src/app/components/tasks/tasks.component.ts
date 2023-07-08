683d6a4bde35da1f48b185545f4eacd952f6caba
Kevin Lee <jhssttj@hotmail.com>
2023-07-08 14:03:41 -0600

    Generate service to put tasks into backend

--- test-1/src/app/components/tasks/tasks.component.ts
+++ test-1/src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { TASKS } from '../../mock-tasks';
 import {Task} from '../../Task';
+import {TaskService} from '../../services/task.service'
 
 @Component({
   selector: 'app-tasks',
@@ -8,6 +8,12 @@ import {Task} from '../../Task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=> this.tasks=tasks);
+  }
 
 }
