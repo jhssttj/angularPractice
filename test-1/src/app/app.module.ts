a4ff32b9cd709ddae4f39a627016f312fbad3b26
Kevin Lee <jhssttj@hotmail.com>
2023-07-09 14:09:43 -0600

    Connect api backend to frontend

--- test-1/src/app/app.module.ts
+++ test-1/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
@@ -18,7 +18,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- test-1/src/app/services/task.service.ts
+++ test-1/src/app/services/task.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import {Observable, of} from 'rxjs'
 import { TASKS } from '../mock-tasks';
 import {Task} from '../Task';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
-  constructor() { }
+  private apiURL = 'http://localhost:5000/tasks'
+  constructor(private http:HttpClient) { }
 
   getTasks() : Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiURL);
   }
 }
