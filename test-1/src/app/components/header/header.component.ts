62b00e963de72f76f0d70c49e2578deecbb5c39a
Kevin Lee <jhssttj@hotmail.com>
2023-07-13 20:12:43 -0600

    Import toggle function from service to function in html

--- test-1/src/app/components/header/header.component.ts
+++ test-1/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {UiService} from '../../services/ui.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +9,12 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   title: string="Task Tracker";
+  showAddTask!: boolean;
+  subscription!: Subscription;
+
+  constructor(private uiService:UiService) {}
+
   toggleAddTask() {
-    console.log('toggle')
+    this.uiService.toggleAddTask();
   }
 }
